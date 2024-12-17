959c58025067bd36b6fe624b833318c190b9bbd0
AnitaSeetaramBhat <anse23mca@cmrit.ac.in>
2024-12-17 13:34:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ from unittest import main
 
 # Test your function by calling it here
 demographic_data_analyzer.calculate_demographic_data()
-
 # Run unit tests automatically
 main(module='test_module', exit=False)
